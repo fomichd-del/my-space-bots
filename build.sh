d86d6f393336516da9ca3450053edd7345cf14bb
fomichd-del <fomichd@gmail.com>
2026-04-30 10:27:09 +0300

    Update build.sh

--- build.sh
+++ build.sh
@@ -1,13 +1,12 @@
 #!/usr/bin/env bash
-# Останавливаем сборку при любой ошибке
 set -o errexit
 
-# 1. Устанавливаем библиотеки
-pip install -r requirements.txt
+# 1. Устанавливаем библиотеки из твоего специального файла
+# Мы просто перенесли эту команду сюда
+pip install -r render_reqs.txt
 
-# 2. Проверяем наличие папки data
+# 2. Создаем папку для данных
 mkdir -p data
 
-# 3. Скачиваем большой файл звезд прямо на диск сервера
-# Флаг -nc (no-clobber) не даст скачивать файл заново, если он уже есть
+# 3. Скачиваем большой файл звезд прямо на диск
 wget -nc -O data/stars.bigksy.0.1.3.mag11.parquet https://github.com/steveberardi/starplot-bigsky/releases/download/v0.1.3/stars.bigksy.0.1.3.mag11.parquet
